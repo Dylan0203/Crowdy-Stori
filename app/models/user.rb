b71cc4efb377a68cf74cfc68f88c5a8e06fcfc77
Dylan Lin <dylanmail0203.gmail>
2016-01-06 22:52:32 +0800

    gem 'omniauth-facebook'

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,58 @@ class User < ActiveRecord::Base
   ROLE_ADMIN = 'admin'
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :chapters
+
+  def get_profile
+    if self.profile
+      return self.profile    
+    else
+      # p = Profile.new
+      # p.user = self
+      # p.save
+      return self.create_profile
+    end
+  end
+
+  def get_fb_data
+    j = RestClient.get "https://graph.facebook.com/v2.5/me", :params => { :access_token => self.fb_token, :fields => "id,name,email,picture" }
+    JSON.parse(j)
+  end
+
+
+  def self.from_omniauth(auth)
+     # Case 1: Find existing user by facebook uid
+     user = User.find_by_fb_uid( auth.uid )
+     if user
+        user.fb_token = auth.credentials.token
+        #user.fb_raw_data = auth
+        user.save!
+       return user
+     end
+
+     # Case 2: Find existing user by email
+     existing_user = User.find_by_email( auth.info.email )
+     if existing_user
+       existing_user.fb_uid = auth.uid
+       existing_user.fb_token = auth.credentials.token
+       #existing_user.fb_raw_data = auth
+       existing_user.save!
+       return existing_user
+     end
+
+     # Case 3: Create new password
+     user = User.new
+     user.fb_uid = auth.uid
+     user.fb_token = auth.credentials.token
+     user.email = auth.info.email
+     user.password = Devise.friendly_token[0,20]
+     #user.fb_raw_data = auth
+     user.save!
+     return user
+  end
 
   def short_email
   self.email.split("@").first
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106122910) do
+ActiveRecord::Schema.define(version: 20160106142857) do
 
   create_table "chapters", force: :cascade do |t|
     t.string   "topic"
@@ -44,9 +44,12 @@ ActiveRecord::Schema.define(version: 20160106122910) do
     t.string   "role"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "fb_uid"
+    t.string   "fb_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["fb_uid"], name: "index_users_on_fb_uid"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
