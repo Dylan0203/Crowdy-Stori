868066d9d8bba59499b6e3fffbc53003040dbc0b
Dylan Lin <dylanmail0203.gmail>
2016-01-11 21:25:24 +0800

    add .json http://139.162.22.162/api/v1/chapters get # post #

--- app/controllers/api_v1/chapters_controller.rb
+++ app/controllers/api_v1/chapters_controller.rb
@@ -1,5 +1,7 @@
 class ApiV1::ChaptersController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
+
   # GET /api/v1/chapters/:id
   def show
     @chapter = Chapter.find(params[:id])
@@ -13,6 +15,22 @@ class ApiV1::ChaptersController < ApplicationController
     @chapters = Chapter.all
 
   # index.json.jbuilder
+  end
+
+  # POST /api/v1/chapters
+  def create
+    @chapter = Chapter.new( :topic => params[:topic],
+                            :setting => params[:setting],
+                            :content => params[:content],
+                            :finish => params[:finish],
+                            :user_id => params[:user_id] )
+    # @topic.user = current_user
+
+    if @chapter.save
+      render :json => { :id => @chapter.id, :message => "OK" }
+    else
+      render :json => { :message => "Invalid", :errors => @chapter.errors }, :status => 400
+    end
   end
 
 end
