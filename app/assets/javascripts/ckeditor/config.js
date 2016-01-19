19c00707c819a7440abc3437aa9aaeab753ba416
Dylan Lin <dylanmail0203@gmail.com>
2016-01-19 21:07:02 +0800

    skip config on ckeditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,11 +1,20 @@
-CKEDITOR.editorConfig = function (config) {
-  // ... other configuration ...
+// CKEDITOR.editorConfig = function (config) {
+//   // ... other configuration ...
 
-  config.toolbar_mini = [
-    ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
-  ];
-  config.toolbar = "simple";
-  config.skin = 'kama';
-  config.extraPlugins='confighelper'; 
-  // ... rest of the original config.js  ...
-}
+//   config.toolbar_mini = [
+//     ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
+//   ];
+//   config.toolbar = "simple";
+//   config.skin = 'kama';
+//   config.extraPlugins='confighelper'; 
+//   // ... rest of the original config.js  ...
+// }
+
+
+// CKEDITOR.replace( 'chapter[content]', {
+//     config.skin = 'kama';
+//     language: 'fr',
+//     uiColor: '#9AB8F3',
+//     skin: 'kama',
+//     extraPlugins:'confighelper'
+// })
