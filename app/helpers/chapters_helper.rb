ea4f3613e3923f5b82e75c988d0b48ce0ba54ca2
Dylan Lin <dylanmail0203@gmail.com>
2016-01-28 11:29:32 +0800

    [CR by ihower] add helper default_chapter_avatar

--- app/helpers/chapters_helper.rb
+++ app/helpers/chapters_helper.rb
@@ -1,2 +1,14 @@
 module ChaptersHelper
+
+	def default_chapter_avatar(chapter)	   
+    case chapter.category.try(:name)
+      when "經典故事"
+        image_tag "fairyTale/#{rand(1..16)}.jpg"
+      when "結局改編"
+        image_tag "reorganization/#{rand(1..14)}.jpg"
+      when "時事創作"
+        image_tag "currentEvent/#{rand(1..12)}.jpg"
+    end
+	end
+
 end
